7ca85758fdfe8cd3b5fa2aad37de423ca9c2aad2
Luis De La Torre <darkknightnadroid@gmail.com>
2023-11-11 23:19:52 -0600

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,12 +18,13 @@ const routes: Routes = [
   },
   {
     path: '**',
+    pathMatch: 'full',
     redirectTo: '404'
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
