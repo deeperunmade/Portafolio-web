d2dd62409450fca641046c9e14569179ecd5228b
Luis De La Torre <darkknightnadroid@gmail.com>
2023-11-12 09:00:49 -0600

    styles coreccion

--- src/app/portafolio/pages/proyectos/proyectos.component.ts
+++ src/app/portafolio/pages/proyectos/proyectos.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
-  styles: [
+  styles: [`
+
+  .card:hover {
+  box-shadow: 5px 5px 20px #263D92;
+  transform: translateY(-3%);
+  }
+
+  `
   ]
 })
 export class ProyectosComponent {
